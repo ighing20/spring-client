621ee0a10f9d79f6ac154c51c74f4c5a3ff81e60
emmanuel-sr <e.sirvasrueda@gmail.com>
2020-04-07 10:51:06 -0500

    add findAll method

--- src/main/java/com/ighing/springclient/RestClient.java
+++ src/main/java/com/ighing/springclient/RestClient.java
@@ -1,5 +1,8 @@
 package com.ighing.springclient;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
@@ -14,14 +17,16 @@ public class RestClient {
 
 	public static void main(String[] args) {
 
-		System.out.println(finOne(1));
-		
-		Cliente cliente = new Cliente();
-		cliente.setNombre("Juan");
-		cliente.setDireccion("Main Street");
-		cliente.setTelefono("555-555-5555");
-		
-		addCliente(cliente);
+		// System.out.println(finOne(1));
+
+		/*
+		 * Cliente cliente = new Cliente(); cliente.setNombre("Juan");
+		 * cliente.setDireccion("Main Street"); cliente.setTelefono("555-555-5555");
+		 * 
+		 * addCliente(cliente);
+		 */
+
+		System.out.println(findAll());
 
 	}
 
@@ -33,4 +38,10 @@ public class RestClient {
 		HttpEntity<Cliente> request = new HttpEntity<>(cliente);
 		REST_TEMPLATE.postForObject(RESOURCE_URL, request, Cliente.class);
 	}
+
+	public static List<Cliente> findAll() {
+		Cliente[] clientes = REST_TEMPLATE.getForEntity(RESOURCE_URL, Cliente[].class).getBody();
+		return Arrays.asList(clientes);
+	}
+
 }
